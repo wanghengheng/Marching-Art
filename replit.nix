1a2aa89c2b8eec828f3adacb51aa7859b802f709
wangyuanchuang@qq.com <wangyuanchuang@qq.com>
2023-10-16 17:07:46 +0800

    init

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-	deps = [
-		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-		pkgs.nodePackages.svelte-language-server
-        pkgs.yarn
-        pkgs.replitPackages.jest
-	];
-}
